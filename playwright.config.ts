4189184f4c56430a5f484c0fc291e51022b54138
AliRaza <ali.raza@etaplus.energy>
2023-11-17 01:53:08 +0100

    Initial commit, project setup completed. Cucumber BDD approach followed.

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,16 +4,6 @@ export const config:LaunchOptions = {
   
     timeout: 600000,
     slowMo: 300,
-    headless: false,
+    headless: true,
   
 };
-
-export default defineConfig({
-    use: {
-      // Emulates the user locale.
-      locale: 'en-GB',
-  
-      // Emulates the user timezone.
-      timezoneId: 'Europe/Paris',
-    },
-  });
